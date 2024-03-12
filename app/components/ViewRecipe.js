079841c287aff1d3f24639d202ed89578ce4df53
Jacey Davis <jaceyd923@gmail.com>
2024-03-12 16:20:23 -0600

    layout for viewRecipe

---
+++ app/components/ViewRecipe.js
@@ -0,0 +1,48 @@
+'use client'
+
+export function ViewRecipe({ recipeToView, setRecipeToView, setInViewMode, recipes, setRecipes }) {
+    console.log(recipeToView.steps)
+
+    return (
+        <section className='max-w-5xl m-auto p-8'>
+            <div className={`h-36 bg-[url('/${recipeToView.imageURL}')] bg-cover bg-center`}></div>
+
+            <div className="grid min-[1000px]:grid-cols-3">
+                <div className="bg-primary text-white p-5 flex flex-col gap-5">
+                    <h2 className="font-bold text-2xl">Ingredients</h2>
+
+                    <ul className="list-disc ml-5">
+                        {recipeToView.ingredients.map((ingredient, i) => (
+                            <li key={i} >{ingredient}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="p-5 flex flex-col gap-5 min-[1000px]:col-span-2">
+                    <div>
+                        <h1 className="text-secondary font-bold text-3xl">{recipeToView.name}</h1>
+                        <p className="italic text-accent font-sm">Source: {recipeToView.source}</p>
+                    </div>
+
+                    <div><button className="btn font-normal font-lato btn-sm btn-outline btn-primary">ADD TO FAVORITES</button></div>
+
+                    <h2 className="font-bold text-2xl">Steps</h2>
+
+                    <ol className="list-decimal ml-5 flex flex-col gap-3">
+                        {recipeToView.steps.map((step, i) => (
+                            <li key={i}>{step}</li>
+                        ))}
+                    </ol>
+
+                    <div>
+                        <button className="btn btn-sm font-normal font-lato m-2">EDIT</button>
+                        <button className="btn btn-sm font-normal font-lato m-2">DELETE</button>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </section>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 'use client'
 import recipeData from './data/recipes.json'
 import { useState } from 'react';
+import { ViewRecipe } from './components/ViewRecipe';
 
 export default function Home() {
     const categories = [...new Set(recipeData.map(recipe => recipe.category))];
     const [recipes, setRecipes] = useState(recipeData)
+    const [inViewMode, setInViewMode] = useState(false)
+    const [recipeToView, setRecipeToView] = useState({})
 
     const filterRecipes = (c) => {
         if (c === 'all') {
@@ -16,36 +19,50 @@ export default function Home() {
         }
     }
 
+    const openRecipe = (recipe) => {
+        setRecipeToView(recipe)
+        setInViewMode(true)
+        window.scrollTo(0, 0);
+    }
+
     return (
         <main>
             <div className="h-[70px]"></div>
 
-            <div className="max-w-5xl m-auto p-8 flex flex-col gap-5">
-                <h1 className="text-secondary text-3xl font-bold">All Recipes</h1>
+            {!inViewMode ? (
+                <section className='max-w-5xl m-auto p-8 flex flex-col gap-5'>
+                    <h1 className="text-secondary text-3xl font-bold">All Recipes</h1>
 
-                <div className="filter-btns">
-                    <input className="join-item btn btn-outline font-normal" type="radio" name="categories" aria-label="all" onClick={() => filterRecipes('all')} />
-                    {categories.map((category, i) => (
-                        <input key={i} className="join-item btn btn-outline font-normal" type="radio" name="categories" aria-label={category} onClick={() => filterRecipes(category)}/>
-                    ))}
-                    <input className="join-item btn btn-outline font-normal" type="radio" name="categories" aria-label="favorites" onClick={() => filterRecipes('favorites')}/>
-                </div>
+                    {/* Filter by Category Buttons */}
+                    <div className="filter-btns flex flex-col flex-wrap min-[575px]:flex-row">
+                        <input className="btn btn-outline font-normal" type="radio" name="categories" aria-label="all" onClick={() => filterRecipes('all')} defaultChecked />
+                        {categories.map((category, i) => (
+                            <input key={i} className="btn btn-outline font-normal" type="radio" name="categories" aria-label={category} onClick={() => filterRecipes(category)} />
+                        ))}
+                        <input className="btn btn-outline font-normal" type="radio" name="categories" aria-label="favorites" onClick={() => filterRecipes('favorites')} />
+                    </div>
 
-                <div className='grid grid-cols-2 gap-5'>
-                    {recipes.map((recipe) => (
-                        <figure key={recipe.id} className='shadow-md cursor-pointer hover:shadow-2xl transition-all'>
-                            <img src={recipe.imageURL} alt={recipe.name}/>
-                            <figcaption className='p-3 flex flex-col gap-2'>
-                                <h2 className='font-bold text-lg'>{recipe.name}</h2>
-                                <div className='flex justify-between text-sm'>
-                                    <p className='text-primary font-lato'>{recipe.category.toUpperCase()}</p>
-                                    <p className='text-accent'>{recipe.time}</p>
-                                </div>
-                            </figcaption>
-                        </figure>
-                    ))}
-                </div>
-            </div>
+                    {/* List of Recipes */}
+                    <div className='grid min-[575px]:grid-cols-2 lg:grid-cols-3 gap-6'>
+                        {recipes.map((recipe) => (
+                            <figure key={recipe.id} className='shadow-md cursor-pointer hover:shadow-2xl transition-all' onClick={() => openRecipe(recipe)}>
+                                <img src={recipe.imageURL} alt={recipe.name} />
+                                <figcaption className='p-5 flex flex-col gap-2'>
+                                    <h2 className='font-bold text-lg'>{recipe.name}</h2>
+                                    <div className='flex justify-between text-sm'>
+                                        <p className='text-primary font-lato'>{recipe.category.toUpperCase()}</p>
+                                        <p className='text-accent'>{recipe.time}</p>
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        ))}
+                    </div>
+                </section>
+            ) : (
+                <ViewRecipe recipeToView={recipeToView} setRecipeToView={setRecipeToView} setInViewMode={setInViewMode} recipes={recipes} setRecipes={setRecipes} />
+            )}
+
+
 
         </main>
     )
